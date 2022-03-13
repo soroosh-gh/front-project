da39e36564637bf58dcab471fe2c3c484575c3cb
soroosh-gh <101410447+soroosh-gh@users.noreply.github.com>
2022-03-13 23:37:46 +0330

    Create Edit.php

---
+++ Edit.php
@@ -0,0 +1,22 @@
+<?php
+
+
+if( isset( $_POST['submit'] ) ){ 
+    $id = $_GET['id'];
+    $dbc = new mysqli('localhost','root','','members');
+    $dbc -> set_charset('utf8_mb4');
+    $sql = "UPDATE user SET
+            name = '{$_POST['name']}',
+            username = '{$_POST['username']}',
+            password = '{$_POST['password']}'
+            WHERE iccode = {$_GET['idcode']}";
+    $result = $dbc -> query( $sql );
+    $dbc -> close();
+
+    echo 'با موفقیت درج شد';
+}
+else{
+
+    include 'edit.php';
+}
+?>
